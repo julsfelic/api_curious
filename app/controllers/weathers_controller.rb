d610a6ee2c9f4b239beee9b63f6bf39ea36c9905
Julian Feliciano <julsfelic@gmail.com>
2016-04-06 20:55:40 -0600

    Setup path for searching for weather

---
+++ app/controllers/weathers_controller.rb
@@ -0,0 +1,4 @@
+class WeathersController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
 
+  resource :weather, only: [:new]
+
   get "/auth/twitter", as: :twitter_login
   get "/auth/twitter/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
